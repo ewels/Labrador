67bf83c290157f884f82c405d82a3daf7ee27f56
Steven Wingett <steven.wingett@babraham.ac.uk>
2021-03-07 18:47:21 +0000

    Improved sendmai setup Updated instructions to use a new config file to setup sendmail.

---
+++ conf/sendmail_config.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+######################################
+# sendmail config
+#
+# This file is for configuring the Labrador
+# email using the software sendmail.
+#
+# Edit the lines of below code needing ATTENTION
+# as described.
+######################################
+
+apt-get install -q -y ssmtp mailutils
+
+# Root is the person who gets all mail for userids < 1000
+
+################################################################
+# ATTENTION: Change to your admin email address here
+echo "root=top.dog@labrador.ac.uk" >> /etc/ssmtp/ssmtp.conf      
+################################################################
+
+################################################################
+# ATTENTION: Change to your mail server name here (remember to keep ':587')
+echo "mailhub=mail.labrador.internal:587" >> /etc/ssmtp/ssmtp.conf   
+################################################################
+
+# Alternatively use address/password
+#RUN echo "AuthUser=your@gmail.com" >> /etc/ssmtp/ssmtp.conf
+#RUN echo "AuthPass=yourGmailPass" >> /etc/ssmtp/ssmtp.conf
+
+echo "UseTLS=YES" >> /etc/ssmtp/ssmtp.conf
+echo "UseSTARTTLS=YES" >> /etc/ssmtp/ssmtp.conf
+
+# Set up php sendmail config
+mkdir /usr/local/etc/php
+mkdir /usr/local/etc/php/conf.d
+echo "sendmail_path=sendmail -i -t" >> /usr/local/etc/php/conf.d/php-sendmail.ini
+echo "FromLineOverride=YES" >> /etc/ssmtp/ssmtp.conf
+
+################################################################
+# ATTENTION: Change to your username:admin_email:mailserver
+echo "top.dog:top.dog@labradorac.uk:mail.lmb.internal" >> /etc/ssmtp/revaliases 
+################################################################
+
+sendmailconfig
+service sendmail start
+
+################################################################
+# ATTENTION: Change to the admin email address
+echo "Subject:Test mail 4\n\n Labrador test email\n" | sendmail top.dog@labrador.ac.uk
+################################################################
